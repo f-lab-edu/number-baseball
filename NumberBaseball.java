a373bb956e10e430965d22ccfc2fb3e05d456f00
dayoungMM <dalbeetm@gmail.com>
2022-04-29 21:35:52 +0900

    feat: 클래스 및 메인함수 생성

---
+++ NumberBaseball.java
@@ -0,0 +1,11 @@
+public class NumberBaseball {
+    private int s = 0;
+    private int b = 0;
+    private int o = 0;
+    private int round = 0;
+
+    public static void main(String[] args) {
+        System.out.println("number baseball project");
+    }
+
+}
