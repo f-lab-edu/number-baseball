3d994e0b0457af4efcc07820fe4d3da862bbd164
smellhyang <dlgiddk10@gmail.com>
2022-05-01 15:40:01 +0900

    feat: 중복되지 않는 3자리 숫자 랜덤 answer 생성 함수

--- NumberBaseball.java
+++ NumberBaseball.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class NumberBaseball {
     private int s = 0;
     private int b = 0;
@@ -5,7 +8,24 @@ public class NumberBaseball {
     private int round = 0;
 
     public static void main(String[] args) {
-        System.out.println("number baseball project");
+        //System.out.println("number baseball project");
+        NumberBaseball game = new NumberBaseball();
+        game.creatAnswer();
     }
 
+    private void creatAnswer() {
+        //중복되지 않는 3자리 숫자 랜덤 answer 생성 함수
+
+        List<Integer> numlist = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            numlist.add(i);
+        }
+        Collections.shuffle(numlist);
+        numlist = numlist.subList(0,3);
+
+        //출력
+        String answer = numlist.stream().map(Object::toString)
+                .collect(Collectors.joining(", "));
+        System.out.println(answer);
+    }
 }
